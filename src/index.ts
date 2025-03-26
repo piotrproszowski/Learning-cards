42dcba2413076b829bd2eb5e2031c57adb55bdff
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2025-03-26 23:19:19 +0100

    feat(index): create entry point for application components

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import "./styles/main.scss";
+import "./components/study-card";
+import "./components/card-deck";
+import "./components/card-editor";
+import "./components/progress-tracker";
+import "./components/app-header";
